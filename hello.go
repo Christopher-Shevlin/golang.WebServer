a72dd78f4c5636e58c0780c8c9708ef20bc802ed
Christopher Shevlin <47715649+Christopher-Shevlin@users.noreply.github.com>
2021-10-17 16:43:40 +0100

    Commit

--- hello.go
+++ hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
 }
